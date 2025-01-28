7cf4b5144a118288f1318b6c13a29a8826f0c593
balde <cbaldelomar@hotmail.com>
2025-01-28 12:05:59 -0600

    Add Problem Details format for error responses

--- Movies.Api/ConfigureServices.cs
+++ Movies.Api/ConfigureServices.cs
@@ -1,10 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+
 namespace Movies.Api;
 
 public static class ConfigureServices
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        // Adds services for using Problem Details format
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Instance =
+                    $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
 
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+
+                Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+                context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+            };
+        });
 
         return services;
     }
